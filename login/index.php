1572c2bfe68e10737a0d786c7d9e440d54139c2a
pcto5ID <pcto.5id@gmail.com>
2022-04-29 23:02:45 +0200

    Update frontend

--- login/index.php
+++ login/index.php
@@ -1,39 +1,27 @@
 <?php
 
-define('TITLE', "Login");
+define('TITLE', "Comma - Login");
 include '../assets/layouts/header.php';
 check_logged_out();
 ?>
 
+<div class="centered-axis-x">
+    <img src="../assets/images/logo_intero_w.png" alt="" height="100px">
+</div>
 
-<div class="container">
-    <div class="row">
-        <div class="col-sm-4">
+<div class="row h-100">
+    <!--- Left Column --->
+    <div class="col-lg-6 col-md-12" id="left_col">
 
-        </div>
-        <div class="col-sm-4">
-            <form class="form-auth" action="includes/login.inc.php" method="post">
-
+    </div>
+    <!-- Right Column -->
+    <div class="row col-lg-6 col-md-12" id="right_col">
+        <div class="col-2" id=""></div>
+        <div class="col-8" id="">
+            <form class="form-auth" action="../includes/login_include" method="POST">
                 <?php insert_csrf_token(); ?>
-
-                <div class="text-center">
-                    <img class="mb-1" src="../assets/images/logo.png" alt="" width="130" height="130">
-                </div>
-
-                <h6 class="h3 mb-3 font-weight-normal text-muted text-center">Login to your Account</h6>
-
-                <div class="text-center mb-3">
-                    <small class="text-success font-weight-bold">
-                        <?php
-                            if (isset($_SESSION['STATUS']['loginstatus']))
-                                echo $_SESSION['STATUS']['loginstatus'];
-
-                        ?>
-                    </small>
-                </div>
-
                 <div class="form-group">
-                    <label for="username" class="sr-only">Username</label>
+                    <label for="username" class="sr-only text-light">Username</label>
                     <input type="text" id="username" name="username" class="form-control" placeholder="Username" required autofocus>
                     <sub class="text-danger">
                         <?php
@@ -42,45 +30,12 @@ check_logged_out();
                         ?>
                     </sub>
                 </div>
-
-                <div class="form-group">
-                    <label for="password" class="sr-only">Password</label>
-                    <input type="password" id="password" name="password" class="form-control" placeholder="Password" required>
-                    <sub class="text-danger">
-                        <?php
-                            if (isset($_SESSION['ERRORS']['wrongpassword']))
-                                echo $_SESSION['ERRORS']['wrongpassword'];
-                        ?>
-                    </sub>
-                </div>
-
-                <div class="col-auto my-1 mb-4">
-                    <div class="custom-control custom-checkbox mr-sm-2">
-                        <input type="checkbox" class="custom-control-input" id="rememberme" name="rememberme">
-                        <label class="custom-control-label" for="rememberme">Remember me</label>
-                    </div>
-                </div>
-
-                <button class="btn btn-lg btn-primary btn-block" type="submit" value="loginsubmit" name="loginsubmit">Login</button>
-
-                <p class="mt-3 text-muted text-center"><a href="../reset-password/">forgot password?</a></p>
-
-                <p class="mt-4 mb-3 text-muted text-center">
-                    <a href="https://github.com/msaad1999/PHP-Login-System" target="_blank">
-                        Login System
-                    </a> | 
-                    <a href="https://github.com/msaad1999/PHP-Login-System/blob/master/LICENSE" target="_blank">
-                        MIT License
-                    </a>
-                </p>
-                
             </form>
         </div>
-        <div class="col-sm-4">
-
-        </div>
+        <div class="col-2" id=""></div>
     </div>
 </div>
+
 
 
 <?php
